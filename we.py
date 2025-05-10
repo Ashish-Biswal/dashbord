85500180e6aad03671c21fb538ef16baab72ed40
Ashish-Biswal <asishbiswal410@gmail.com>
2025-05-10 21:39:00 +0530

    Initial project files

---
+++ we.py
@@ -0,0 +1,14 @@
+# class Animal:
+#     def speak(self):
+#         print("Sound")
+
+# class Dog(Animal):
+#     pass
+
+# d = Dog()
+# d.speak()  # Output: Sound
+
+
+my_list = [1,2,2,3,1,1,3,3]
+unique = list(set(my_list))  # [1, 2, 3]
+
